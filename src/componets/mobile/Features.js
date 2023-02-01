31b06aac8fc99947464af9665655ebe1323801a5
main account <xxiraqgamersxx@gmail.com>
2023-02-01 12:22:07 +0300

    fixed the feature icon padding

--- src/componets/mobile/Features.js
+++ src/componets/mobile/Features.js
@@ -13,19 +13,19 @@ const Features = () => {
         <h3 className="pl-3 text-2xl font-bold">What's special about us</h3>
       </div>
       <Feature
-        imgP="1"
+        padding="p-1"
         img="flower-icon"
         title="Our exquisite flower collection"
         p="Flowers that are hand picked by people who dedicated their life to get you the flowers that you deserve."
       />
       <Feature
-        imgP="3"
+        padding="p-3"
         img="notification-icon"
         title="Informative notification system"
         p="with our notification system you`ll get personal email`s on the day of the event containing all the information you need with a picture of the flower`s you are giving."
       />
       <Feature
-        imgP="2"
+        padding="p-2"
         img="delivery-icon"
         title="Fast and reliable delivery"
         p="once you setup Flowery no need to every worry about the delivery time and efficiency. We got "
--- src/componets/mobile/parts/Feature.js
+++ src/componets/mobile/parts/Feature.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const Feature = (props) => {
   const img = `images/${props.img}.svg`;
+  // const imgPadding = `p-${props.padding}`;
   return (
     <div className="pl-3 flex flex-col items-center justify-center gap-4">
-      <div className={`w-16 h-16 bg-[#D0B8AC] rounded-full p-${props.imgP}`}>
+      <div
+        className={`w-16 h-16 bg-[#D0B8AC] rounded-full ${props.padding} overflow-hidden`}
+      >
         <img src={img} alt="" />
       </div>
       <h4 className="text-left text-xl font-semibold">{props.title}</h4>
